832a7442fa0880d375fff6e98dfcdc75dd0d2cc2
Doug K <doug.kerzner@gmail.com>
2021-01-25 15:35:09 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -9,11 +9,16 @@ def monopoly_with_second_tier
   # you built in the previous method. Run the tests. They will fail, but
   # they'll guide you in how to modify what you just did to have more
   # complexity.
+<<<<<<< HEAD
   {railroads: {
     pieces: 4,
     rent_in_dollars: {},
     names: {}
   }}
+=======
+  {railroads: {}}
+  railroads: {pieces: 4, rent_in_dollars: {}, names: {}}
+>>>>>>> 39db92cc074af37540f733a5097f0161d92fd200
     
 end
 
